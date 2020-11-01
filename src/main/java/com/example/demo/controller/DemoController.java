fd3f9c0a15cd05cad85a90284b66515c21a98ea8
qkyy521314 <qkyy521314@163.com>
2020-11-01 15:00:32 +0800

    log

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,15 +1,18 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
     @RequestMapping("/say/{content}")
     public String say(@PathVariable("content") String content) {
+        log.info("content {}", content);
         return "content is " + content;
     }
 
